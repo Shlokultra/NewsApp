a1a6ed14d10a542e1aad11ff6ae3e27cf906a992
shlok <yadavshlok2005@gmail.com>
2026-05-23 21:24:54 +0530

    Passed images through props

--- src/components/NewsItem.jsx
+++ src/components/NewsItem.jsx
@@ -4,19 +4,20 @@ import Card from 'react-bootstrap/Card';
 import React, { Component } from 'react'
 
 export class NewsItem extends Component {
+  
   render() {
-    let {title,description} = this.props;
+    let {title,description,imageurl,newsurl} = this.props;
     return (
-      <div>
+      <div className='my-3'>
         <>
           <Card style={{ width: '18rem' }}>
-            <Card.Img variant="top" src="holder.js/100px180" />
+            <Card.Img variant="top" src={imageurl} />
             <Card.Body>
               <Card.Title> {title} </Card.Title>
               <Card.Text>
                 {description}
               </Card.Text>
-              <Button variant="primary">Go somewhere</Button>
+              <Button href='/newsdetail/' variant="primary">Read More</Button>
             </Card.Body>
           </Card>
         </>
