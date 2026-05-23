40231db3117b8148e0a8e949f288d75de081c9b8
shlok <yadavshlok2005@gmail.com>
2026-05-23 14:39:44 +0530

    Modfified to use api efficiently

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -3,9 +3,25 @@ import NewsItem from './NewsItem'
 export class News extends Component {
   render() {
     return (
-      <div>
-        This is a news component 
-        <NewsItem />
+      <div className='container my-3'>
+        <>
+          <h2>InfoSentinel News - Top Headlines</h2>
+          <div className="row">
+            <div className="col-md-4">
+              <NewsItem title="mytitle" description="mydesc" />
+            </div>
+
+            <div className="col-md-4">
+              <NewsItem title="mytitle" description="mydesc" />
+            </div>
+
+            <div className="col-md-4">
+              <NewsItem title="mytitle" description="mydesc" />
+            </div>
+
+            
+          </div>
+        </>
       </div>
     )
   }
--- src/components/NewsItem.jsx
+++ src/components/NewsItem.jsx
@@ -1,10 +1,25 @@
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
 import React, { Component } from 'react'
 
 export class NewsItem extends Component {
   render() {
+    let {title,description} = this.props;
     return (
       <div>
-        This is a news item components 
+        <>
+          <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src="holder.js/100px180" />
+            <Card.Body>
+              <Card.Title> {title} </Card.Title>
+              <Card.Text>
+                {description}
+              </Card.Text>
+              <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+          </Card>
+        </>
       </div>
     )
   }
