8ded7dd1b5a354546f77f3eab3e27b2a25920b16
shlok <yadavshlok2005@gmail.com>
2026-05-27 14:50:33 +0530

    Added date author and sources of the news to authenticate things.

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -122,7 +122,7 @@ export class News extends Component {
           <div className="row">
             {!this.state.loading && this.state.articles.map((element) => {
               return <div className="col-md-4" key={element.imageurl}>
-                <NewsItem title={element.title ? element.title : ""} description={element.description ? element.description : ""} imageurl={element.urlToImage} newsurl={element.url} />
+                <NewsItem title={element.title ? element.title : ""} description={element.description ? element.description : ""} imageurl={element.urlToImage} newsurl={element.url} author={element.author} date={element.publishedAt} source={element.source.name} />
               </div>
             })}
 
--- src/components/NewsItem.jsx
+++ src/components/NewsItem.jsx
@@ -1,22 +1,25 @@
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import Badge from 'react-bootstrap/Badge';
 
 import React, { Component } from 'react'
 
 export class NewsItem extends Component {
 
   render() {
-    let { title, description, imageurl, newsurl } = this.props;
+    let { title, description, imageurl, newsurl, author, date, source  } = this.props;
     return (
       <div className='my-3'>
         <>
           <Card >
             <Card.Img variant="top" src={!imageurl ? "https://cdn.arstechnica.net/wp-content/uploads/2026/05/starshipflight12_inflight-1152x648.jpg" : imageurl} />
             <Card.Body>
-              <Card.Title> {title} </Card.Title>
+              <Card.Title> {title} <Badge bg="secondary"> source: {source} </Badge>
+                <span className="visually-hidden">unread messages</span> </Card.Title>
               <Card.Text>
                 {description}
               </Card.Text>
+              <Card.Text>By {!author ? "Unknown" : author} on {new Date(date).toGMTString()} </Card.Text>
               <Button href={newsurl} target='_blank' variant="primary">Read More</Button>
             </Card.Body>
           </Card>
