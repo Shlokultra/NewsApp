9b033d9d9f88fea677cafbdda0deea08caad9d19
shlok <yadavshlok2005@gmail.com>
2026-05-27 15:09:14 +0530

    Added light themed background

--- src/components/NewsItem.jsx
+++ src/components/NewsItem.jsx
@@ -7,14 +7,16 @@ import React, { Component } from 'react'
 export class NewsItem extends Component {
 
   render() {
-    let { title, description, imageurl, newsurl, author, date, source  } = this.props;
+    let { title, description, imageurl, newsurl, author, date, source } = this.props;
     return (
       <div className='my-3'>
         <>
           <Card >
             <Card.Img variant="top" src={!imageurl ? "https://cdn.arstechnica.net/wp-content/uploads/2026/05/starshipflight12_inflight-1152x648.jpg" : imageurl} />
             <Card.Body>
-              <Card.Title> {title} <Badge bg="secondary"> source: {source} </Badge>
+              <Card.Title> {title} <Badge bg="success" text="dark">
+                 source: {source}
+              </Badge>
                 <span className="visually-hidden">unread messages</span> </Card.Title>
               <Card.Text>
                 {description}
